845b53f649f45ba765c85c2ba59db914ea540685
ParnaChatterjee-Grid <pchatterjee@griddynamics.com>
2025-01-27 03:15:19 +0530

    Initial setup- empty multimodule with detekt.

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -32,15 +32,12 @@ android {
         jvmTarget = "17"
     }
 }
-
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.dagger)
     ksp(libs.dagger.compiler)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
