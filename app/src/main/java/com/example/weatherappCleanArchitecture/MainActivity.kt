f221c190022b38215c7119920a2ae7281ee8a4c2
ParnaChatterjee-Grid <pchatterjee@griddynamics.com>
2025-02-03 10:23:22 +0530

    Remove commented line and the empty WeatherAppCleanArchitectureTheme theme .

--- app/src/main/java/com/example/weatherappCleanArchitecture/MainActivity.kt
+++ app/src/main/java/com/example/weatherappCleanArchitecture/MainActivity.kt
@@ -2,9 +2,6 @@ package com.example.weatherappCleanArchitecture
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import com.example.weatherappCleanArchitecture.ui.theme.WeatherAppCleanArchitectureTheme
 
 
 class MainActivity : ComponentActivity() {
