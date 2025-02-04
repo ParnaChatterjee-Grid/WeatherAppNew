20fe7c12f7253b5f0db8bafb41296a377bc98780
ParnaChatterjee-Grid <pchatterjee@griddynamics.com>
2025-02-04 15:35:02 +0530

    Remove commented code and Adding viewmodels.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,29 +41,6 @@ android {
 }
 
 dependencies {
-   /* implementation(project(":common"))
-    implementation(project(":data"))
-    implementation(project(":domain"))
-    implementation(project(":presentation"))
-
-    implementation(libs.dagger)
-    ksp(libs.dagger.compiler)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)*/
-
     implementation(project(":common"))
     implementation(project(":data"))
     implementation(project(":domain"))
